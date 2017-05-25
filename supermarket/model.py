5db08ddba7a2eb77ac48fabf18c75e8d8064089e
Maya Kokits <maya@more-onion.com>
2017-05-26 01:11:48 +0200

    add tables and relationships

--- supermarket/model.py
+++ supermarket/model.py
@@ -1,17 +1,49 @@
+import json
+import sqlalchemy
+
 from moflask.flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.types import TypeDecorator
 
 
 db = SQLAlchemy()
 
 
+# custom data types
+
+class Serialized(TypeDecorator):
+    impl = sqlalchemy.Text
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
 # helper tables
 
-products_resources = db.Table(
-    'products_resources',
-    db.Column('product_id', db.Integer,
-              db.ForeignKey('product.id'), primary_key=True),
-    db.Column('resource_id', db.Integer,
-              db.ForeignKey('resource.id'), primary_key=True)
+products_resources = db.Table('products_resources',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('resource_id', db.Integer, db.ForeignKey('resources.id'), primary_key=True)
+)
+
+products_labels = db.Table('products_labels',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('label_id', db.Integer, db.ForeignKey('labels.id'), primary_key=True)
+)
+
+products_stores = db.Table('products_stores',
+    db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
+    db.Column('store_id', db.Integer, db.ForeignKey('stores.id'), primary_key=True)
+)
+
+retailers_certificates = db.Table('retailers_certificates',
+    db.Column('retailer_id', db.Integer, db.ForeignKey('retailers.id'), primary_key=True),
+    db.Column('certificate_id', db.Integer, db.ForeignKey('certificates.id'), primary_key=True)
 )
 
 
@@ -21,6 +53,7 @@ class Brand(db.Model):
     __tablename__ = 'brands'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
+    retailer_id = db.Column(db.Integer, db.ForeignKey('retailers.id'))
     products = db.relationship('Product', backref='brand', lazy=True)
 
 
@@ -31,16 +64,63 @@ class Category(db.Model):
     products = db.relationship('Product', backref='category', lazy=True)
 
 
-class Hotspot(db.Model):
-    __tablename__ = 'Hotspots'
+class Certificate(db.Model):
+    __tablename__ = 'certificates'
+    id = db.Column(db.Integer(), primary_key=True)
+    number = db.Column(db.String(8))
+    name = db.Column(db.String(64))
+    details = db.Column(Serialized()) # holds question, response options, explanation, possible scores
+
+
+class CertificateMeetsCriterion(db.Model):
+    __tablename__ = 'certificates_criteria'
+    certificate_id = db.Column('certificate_id', db.Integer, db.ForeignKey('certificates.id'), primary_key=True)
+    criterion_id = db.Column('criterion_id', db.Integer, db.ForeignKey('criteria.id'), primary_key=True)
+    score = db.Column('score', db.Integer)
+    explanation = db.Column('explanation', db.Text)
+    criterion = db.relationship('Criterion', lazy=True)
+
+
+class Criterion(db.Model):
+    __tablename__ = 'criteria'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    hotspots = db.relationship('CriterionInfluencesHotspot', lazy=True)
+
+
+class CriterionInfluencesHotspot(db.Model):
+    __tablename__ = 'criteria_hotspots'
+    criterion_id = db.Column('criterion_id', db.Integer, db.ForeignKey('criteria.id'), primary_key=True)
+    hotspot_id = db.Column('hotspot_id', db.Integer, db.ForeignKey('hotspots.id'), primary_key=True)
+    score = db.Column('score', db.Integer)
+    explanation = db.Column('explanation', db.Text)
+    hotspot = db.relationship('Hotspot', lazy=True)
+
+
+class Hotspot(db.Model):
+    __tablename__ = 'hotspots'
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(256))
+    description = db.Column(db.Text)
 
 
 class Label(db.Model):
     __tablename__ = 'labels'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
+    description = db.Column(db.Text)
+    logo = db.Column(db.String(256))
+    criteria = db.relationship('LabelMeetsCriterion', lazy=True)
+
+
+class LabelMeetsCriterion(db.Model):
+    __tablename__ = 'labels_criteria'
+    label_id = db.Column('label_id', db.Integer, db.ForeignKey('labels.id'), primary_key=True)
+    criterion_id = db.Column('criterion_id', db.Integer, db.ForeignKey('criteria.id'), primary_key=True)
+    score = db.Column('score', db.Integer)
+    explanation = db.Column('explanation', db.Text)
+    criterion = db.relationship('Criterion', lazy=True)
 
 
 class Origin(db.Model):
@@ -60,12 +140,19 @@ class Product(db.Model):
     __tablename__ = 'products'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
-    brand_id = db.Column(db.Integer, db.ForeignKey('brand.id'))
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-    producer_id = db.Column(db.Integer, db.ForeignKey('producer.id'))
-    store_id = db.Column(db.Integer, db.ForeignKey('store.id'))
+    brand_id = db.Column(db.Integer, db.ForeignKey('brands.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    producer_id = db.Column(db.Integer, db.ForeignKey('producers.id'))
     resources = db.relationship(
         'Resource', secondary=products_resources,
+        lazy='subquery', backref=db.backref('products', lazy=True)
+    )
+    labels = db.relationship(
+        'Label', secondary=products_labels,
+        lazy='subquery', backref=db.backref('products', lazy=True)
+    )
+    stores = db.relationship(
+        'Store', secondary=products_stores,
         lazy='subquery', backref=db.backref('products', lazy=True)
     )
 
@@ -74,7 +161,6 @@ class Store(db.Model):
     __tablename__ = 'stores'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
-    products = db.relationship('Product', backref='store', lazy=True)
 
 
 class Supplier(db.Model):
@@ -93,3 +179,9 @@ class Retailer(db.Model):
     __tablename__ = 'retailers'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(64))
+    brands = db.relationship('Brand', backref='retailer', lazy=True)
+    stores = db.relationship('Store', backref='retailer', lazy=True)
+    certificates = db.relationship(
+        'Certificate', secondary=retailers_certificates,
+        lazy='subquery', backref=db.backref('retailers', lazy=True)
+    )
