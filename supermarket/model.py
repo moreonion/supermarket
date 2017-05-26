4b8a30dd34ad6c8c8b5d66967e95e5308b898907
Maya Kokits <maya@more-onion.com>
2017-05-26 16:48:41 +0200

    fix store-retailer relationship

--- supermarket/model.py
+++ supermarket/model.py
@@ -159,6 +159,7 @@ class Product(db.Model):
 class Store(db.Model):
     __tablename__ = 'stores'
     id = db.Column(db.Integer(), primary_key=True)
+    retailer_id = db.Column(db.Integer, db.ForeignKey('retailers.id'))
     name = db.Column(db.String(64))
 
 
