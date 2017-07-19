78d6c7ff2639bc5dd59516daaac643a05c0dfc71
Katharina Zwinger <katharina@more-onion.com>
2017-07-19 15:58:39 +0200

    CORS: allow "Content-Type" headers
    Needed for OPTIONS requests in json format from other servers.

--- supermarket/__init__.py
+++ supermarket/__init__.py
@@ -12,5 +12,5 @@ class App(BaseApp):
         super().__init__(*args, **kwargs)
         db.init_app(self)
         ma.init_app(self)
-        CORS(allow_headers=['Authorization']).init_app(self)
+        CORS(allow_headers=['Authorization', 'Content-Type']).init_app(self)
         self.register_blueprint(api_app, url_prefix='/api/v1')
